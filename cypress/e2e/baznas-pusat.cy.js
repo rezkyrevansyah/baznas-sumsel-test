7f620609a64ba932a7f943b4508cbd0d682e48b1
M. Rezky Revansyah Suprihono <rezky.revansyah@gmail.com>
2024-09-19 10:51:33 +0700

    update script

--- cypress/e2e/baznas-pusat.cy.js
+++ cypress/e2e/baznas-pusat.cy.js
@@ -12,5 +12,13 @@ describe('template spec', () => {
 
     cy.go('back')
     cy.reload()
+
+    cy.get('#owl-news > a > :nth-child(2) > [style="height:50px;line-height: 19px; overflow: hidden;"] > .font-weight-bold').click()
+
+    cy.visit('https://baznas.go.id/')
+
+    cy.get('#owl-program-smalls > :nth-child(1) > .row > .col-8 > a > [style="height:38px;overflow:hidden;"] > .text-capitalize').click()
+
+    cy.visit('https://baznas.go.id/')
   })
 })
