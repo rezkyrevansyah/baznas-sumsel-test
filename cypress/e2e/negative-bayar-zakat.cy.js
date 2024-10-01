ce5e251b9fffbfb90ef7c14228139f5aa0836c8b
M. Rezky Revansyah Suprihono <rezky.revansyah@gmail.com>
2024-10-01 08:48:41 +0700

    update script

--- cypress/e2e/negative-bayar-zakat.cy.js
+++ cypress/e2e/negative-bayar-zakat.cy.js
@@ -9,63 +9,13 @@ describe('Bayar Zakat', () => {
         return false;
     })
 
-    it('Negative Test', () => {
+    it('Negative Test semua kosong', () => {
 
-        cy.visit("https://sumsel.baznas.go.id/bayarzakat#")
-
-        // Jenis dana zakat
-        cy.get('#type_of_fund').select(0)
-
-        cy.get('#jenis').select(0)
-        cy.get('#jenis').select(1)
-
-        // Jenis dana infak
-        cy.get('#type_of_fund').select(1)
-        cy.get('#jenis').select(0)
-
-        // jenis dana fidyah
-        cy.get('#type_of_fund').select(2)
-        cy.get('#jenis').select(0)
-
-        cy.get('#hariid').select(0)
-        cy.get('#hariid').select(1)
-        cy.get('#hariid').select(2)
-        cy.get('#hariid').select(3)
-        cy.get('#hariid').select(4)
-        cy.get('#hariid').select(5)
-        cy.get('#hariid').select(6)
-        cy.get('#hariid').select(7)
-        cy.get('#hariid').select(8)
-        cy.get('#hariid').select(9)
-        cy.get('#hariid').select(10)
-        cy.get('#hariid').select(11)
-        cy.get('#hariid').select(12)
-        cy.get('#hariid').select(13)
-        cy.get('#hariid').select(14)
-        cy.get('#hariid').select(15)
-        cy.get('#hariid').select(16)
-        cy.get('#hariid').select(17)
-        cy.get('#hariid').select(18)
-        cy.get('#hariid').select(19)
-        cy.get('#hariid').select(20)
-        cy.get('#hariid').select(21)
-        cy.get('#hariid').select(22)
-        cy.get('#hariid').select(23)
-        cy.get('#hariid').select(24)
-        cy.get('#hariid').select(25)
-        cy.get('#hariid').select(26)
-        cy.get('#hariid').select(27)
-        cy.get('#hariid').select(28)
-        cy.get('#hariid').select(29)
-
-        // jumlah hari
-        cy.get('#hariid').select(2)
+        cy.visit("https://kotayogya.baznas.go.id/bayarzakat#")
 
         // Jenis dana zakat
-        cy.get('#type_of_fund').select(0)
-
-        cy.get('#jenis').select(0)
-        cy.get('#jenis').select(1)
+        cy.get('#type_of_fund').select(0).should('have.value', 'zakat', 'contains', 'Zakat')
+        cy.get('#jenis').select(0).should('have.value', 'zakatpenghasilan', 'contains', 'Zakat Penghasilan')
 
         // nominal
         cy.get('#nominal').eq(0).type("50000")
@@ -83,9 +33,11 @@ describe('Bayar Zakat', () => {
         cy.get('#email').eq(0)
 
         // button next
-        cy.get('#next-button').click().wait(150)
+        cy.get('#next-button').click()
 
+        // cy.get('#nominal-error').contains("Mohon Masukkan Nominal Donasi")
         cy.get('#user_title-error').contains("This field is required.")
+        cy.get('#name-error').contains("Mohon Masukkan Nama")
         cy.get('#handphone-error').contains("Mohon Masukkan Nomor Handphone Untuk Pengiriman Bukti Bayar")
         cy.get('#email-error').contains("Mohon Masukkan Email untuk pengiriman Bukti Bayar")
 
@@ -97,4 +49,8 @@ describe('Bayar Zakat', () => {
         // cy.get('#header > div.title-bar')
 
     })
+
+    // it('Negative Test', () => {
+
+    // })
 })
